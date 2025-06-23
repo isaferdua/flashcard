a1af90159ead75abe982c0dfad63b27a1345b9e6
isaferdua <isabelle.fernandes.duarte@escola.pr.gov.br>
2025-06-23 16:20:34 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Biologia',
+    'O que é Anfibios?',
+    'Anfíbios são animais vertebrados que vivem entre o meio aquático e o ambiente terrestre'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
