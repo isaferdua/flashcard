7b9e1d912c3d4ff9a20837883d924397b9518f7b
isaferdua <isabelle.fernandes.duarte@escola.pr.gov.br>
2025-06-24 17:55:05 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -20,3 +20,32 @@ criaCartao(
     'Qual o nome do livro que Hitler escreveu?',
     'Mein Kampf.'
 )
+
+criaCartao(
+    'Biologia',
+    'Qual é a Biotecnologia que envolve Medicina?',
+    'É a Biotecnologia Vermelha.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo ?',
+    'Vaticano.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 64?',
+    'A raiz  quadrada de 64, é 8.'
+)
+criaCartao(
+    'Língua Portuguesa',
+    'O que é uma Vogal?',
+    'Em português, uma vogal é um som da fala produzido sem obstrução da passagem do ar pela boca.'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se fala país em Inglês?',
+    'Em Inglês a palavra país se fala country(comtrui).'
+)
